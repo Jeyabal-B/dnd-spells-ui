1d9cfe87e0627efdd98087d710492d000e649e9c
Jeyabal <jeyabal1993@gmail.com>
2026-02-27 00:31:45 -0500

    added search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import './style/spellTable.css'
 function App() {
 
   const [spells, setSpells] = useState<Spell[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<String | null>(null);
 
@@ -22,15 +23,35 @@ function App() {
     }
 
     loadPage();
-  
   }, []);
 
   if(loading) return <p>Loading spells..</p>
   if(error) return <p>Error occured: {error}</p>
 
+  const filteredSpells = spells.filter((spell) => {
+    const term = searchTerm.toLowerCase();
+    return (
+      spell.name.toLowerCase().includes(term) || 
+      spell.level.toString().toLowerCase().includes(term) || 
+      spell.castingTime.toLowerCase().includes(term) || 
+      spell.range.toLowerCase().includes(term) ||
+      spell.school.toLowerCase().includes(term) || 
+      spell.duration.toLowerCase().includes(term) || 
+      spell.source.toLowerCase().includes(term) 
+    );
+  });
+
   return (
     <div className='spellbook-container'>
       <h1>Spellbook</h1>
+
+      <input
+        type='text'  
+        placeholder='Type to filter spells'
+        className='spell-search'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
 
       <table className='spell-table'>
         <thead>
@@ -49,7 +70,7 @@ function App() {
         </thead>
 
         <tbody>
-          {spells.map((spell) => (
+          {filteredSpells.map((spell) => (
             <tr key={spell.id}>
               <td>{spell.name}</td>
               <td>{spell.level}</td>
